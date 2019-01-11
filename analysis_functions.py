langau multiprocess reversed due to wrong results

analysis_functions.py
@@ -21,10 +21,6 @@ import pylandau
 from nb_analysisFunction import *
 from time import time
 from multiprocessing import Pool
-try:
-    import iminuit
-except:
-    print("Iminuit module cannot be loaded")
 
 
 class main_loops:
@@ -603,17 +599,42 @@ class langau:
             valid_events_Signal = np.take(self.data[data]["Signal"], indizes)  # Get the clustersizes of valid events
             # Get events which show only cluster in its data
             size = 0 # Dummy variable for first loop
+            charge_cal, noise = self.main.calibration.charge_cal, self.main.noise
             self.results_dict[data]["Clustersize"] = []
 
-            paramslist = []
-            for size in clustersize_list:
-                paramslist.append((size+1, valid_events_Signal, valid_events_clusters, valid_events_clustersize, self.main.calibration.charge_cal, self.main.noise))
+            #paramslist = []
+            #for size in clustersize_list:
+            #    paramslist.append((size+1, valid_events_Signal, valid_events_clusters, valid_events_clustersize, self.main.calibration.charge_cal, self.main.noise))
 
-            results = self.pool.starmap(langau_cluster, paramslist) # Here multiple cpu calculate the energy of the events per clustersize
-            self.pool.close()
-            self.pool.join()
+            #results = self.pool.starmap(langau_cluster, paramslist) # Here multiple cpu calculate the energy of the events per clustersize
+            #self.pool.close()
+            #self.pool.join()
 
-            self.results_dict[data]["Clustersize"] = results
+            for size in tqdm(clustersize_list, desc="(langau) Processing clustersize"):
+                # get the events with the different clustersizes
+                cls_ind = np.nonzero(valid_events_clustersize == size)[0]
+                # indizes_to_search = np.take(valid_events_clustersize, cls_ind) # TODO: veeeeery ugly implementation
+                totalE = np.zeros(len(cls_ind))
+                totalNoise = np.zeros(len(cls_ind))
+                # Loop over the clustersize to get total deposited energy
+                incrementor = 0
+                for ind in tqdm(cls_ind, desc="(langau) Processing event"):
+                    # TODO: make this work for multiple cluster in one event
+                    # Signal calculations
+                    signal_clst_event = np.take(valid_events_Signal[ind], valid_events_clusters[ind][0])
+                    totalE[incrementor] = np.sum(convert_ADC_to_e(signal_clst_event, charge_cal))
+
+                    # Noise Calculations
+                    noise_clst_event = np.take(noise, valid_events_clusters[ind][0])  # Get the Noise of an event
+                    totalNoise[incrementor] = np.sqrt(np.sum(convert_ADC_to_e(noise_clst_event, charge_cal)))  # eError is a list containing electron signal noise
+
+                    incrementor += 1
+
+                preresults = {}
+                preresults["signal"] = totalE
+                preresults["noise"] = totalNoise
+
+                self.results_dict[data]["Clustersize"].append(preresults)
 
             # With all the data from every clustersize add all together and fit the langau to it
 

utilities.py
@@ -134,8 +134,8 @@ class NoStdStreams(object):
         self.devnull.close()
 
 
-def langau_cluster(size, valid_events_Signal, valid_events_clusters,valid_events_clustersize, charge_cal, noise):
-    # for size in tqdm(cluster_list, desc="(langau) Processing clustersize"):
+def langau_cluster(size, valid_events_Signal, valid_events_clusters, valid_events_clustersize, charge_cal, noise):
+    # for size in tqdm(clustersize_list, desc="(langau) Processing clustersize"):
     # get the events with the different clustersizes
     cls_ind = np.nonzero(valid_events_clustersize == size)[0]
     # indizes_to_search = np.take(valid_events_clustersize, cls_ind) # TODO: veeeeery ugly implementation