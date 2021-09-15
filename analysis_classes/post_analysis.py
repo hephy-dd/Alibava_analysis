68e6f15645038a03de0e07d4a5451d461380b87a
mwiehe <m.wiehe@cern.ch>
2021-09-15 12:09:03 +0200

    create copy of results

--- analysis_classes/post_analysis.py
+++ analysis_classes/post_analysis.py
@@ -1,19 +1,24 @@
 #this script is called from AliSys.py for each set of ped,cal,run files
 
 import pdb
+import copy
 
 def main(ped, cal, run, results):
     #This is executed for each set of files
+    myresults=copy.copy(results)#create copy of results dict
     print('### Scribbling Pad ###')
     print('Keys in \'results\'',results.keys())
     for i in [ped, cal, run]: print(i)
-
+    
     ## put some code here
-    results=[]
-    pdb.set_trace()
+
+    #pdb.set_trace()
     
     print('##########################')
-    return results
+    return myresults
 
-def final(results):
+def final(myresults):
     #This is executed at the end of the analysis
+    
+    print(myresults)
+    return 0
