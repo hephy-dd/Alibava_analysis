350a3d44609819951dbf74e6efb3dd642aa859ac
mwiehe <m.wiehe@cern.ch>
2021-10-18 17:24:11 +0200

    layout

--- analysis_classes/noise_analysis.py
+++ analysis_classes/noise_analysis.py
@@ -7,9 +7,11 @@ import numpy as np
 from tqdm import tqdm
 from analysis_classes.utilities import import_h5, read_binary_Alibava
 
+
 class NoiseAnalysis:
     """This class contains all calculations and data concerning pedestals in
-	ALIBAVA files"""
+    ALIBAVA files"""
+
     def __init__(self, path="", configs=None, logger=None):
         """
         :param path: Path to pedestal file
@@ -34,7 +36,7 @@ class NoiseAnalysis:
             self.noiseNCM = np.zeros(self.numchan, dtype=np.float32)
 
             # Only use events with good timing, here always the case
-            self.noise_cut = configs.get("Noise_cut", 5.)
+            self.noise_cut = configs.get("Noise_cut", 5.0)
             self.which_strips = configs.get("Chips", (1, 2))
             self.max_channels = configs.get("numChan", 256)
             self.mask = configs.get("Manual_mask", [])
@@ -43,8 +45,9 @@ class NoiseAnalysis:
             self.CMnoise_raw = np.zeros(self.numchan, dtype=np.float32)
             self.CMsig = np.zeros(self.numchan, dtype=np.float32)
             self.CMsig_raw = np.zeros(self.numchan, dtype=np.float32)
-            self.score = np.zeros((len(self.goodevents[0]), self.numchan),
-                                  dtype=np.float32)
+            self.score = np.zeros(
+                (len(self.goodevents[0]), self.numchan), dtype=np.float32
+            )
             self.median_noise = None
 
             # Calculate pedestal
@@ -54,28 +57,41 @@ class NoiseAnalysis:
             # complete signal matrix (event vs. channel)
             self.signal = np.array(self.data["events"]["signal"][:], dtype=np.float32)
 
-
             # First Noise calculation without masking to get an idea of the data
-            self.noise_raw, self.noiseNCM_raw, self.CMnoise_raw, self.CMsig_raw = \
-                    self.noise_calc(self.signal, self.pedestal)
-            self.noisy_strips, self.good_strips = \
-                        self.detect_noisy_strips(self.noise_raw, self.noise_cut)
+            (
+                self.noise_raw,
+                self.noiseNCM_raw,
+                self.CMnoise_raw,
+                self.CMsig_raw,
+            ) = self.noise_calc(self.signal, self.pedestal)
+            self.noisy_strips, self.good_strips = self.detect_noisy_strips(
+                self.noise_raw, self.noise_cut
+            )
             # Mask chips of alibava
-            self.chip_selection, self.masked_channels = \
-                    self.mask_alibava_chips(self.which_strips, self.max_channels)
+            self.chip_selection, self.masked_channels = self.mask_alibava_chips(
+                self.which_strips, self.max_channels
+            )
             # Redefine good strips and noisy strips
             self.good_strips = np.intersect1d(self.chip_selection, self.good_strips)
-            self.noisy_strips = np.append(self.noisy_strips,self.masked_channels)
-            noise_corr, noiseNCM_corr, self.CMnoise, self.CMsig, self.total_noise = \
-                        self.noise_calc(self.signal[:, self.good_strips],
-                                      self.pedestal[self.good_strips], True)
+            self.noisy_strips = np.append(self.noisy_strips, self.masked_channels)
+            (
+                noise_corr,
+                noiseNCM_corr,
+                self.CMnoise,
+                self.CMsig,
+                self.total_noise,
+            ) = self.noise_calc(
+                self.signal[:, self.good_strips], self.pedestal[self.good_strips], True
+            )
 
             # self.noise is only the non masked strips long. Make it to the full 256 strips long array so we can use it
             # Insert the correct noise for the masked strips and for all else insert np.nan --> This way it raises an error
             # if someone tries to access and claculate with it
             it = 0
             for i in range(self.numchan):
-                if i in self.good_strips: # If its a good strip then add it and increase the it for tracking
+                if (
+                    i in self.good_strips
+                ):  # If its a good strip then add it and increase the it for tracking
                     self.noise[i] = noise_corr[it]
                     self.noiseNCM[i] = noiseNCM_corr[it]
                     it += 1
@@ -83,23 +99,20 @@ class NoiseAnalysis:
                     self.noise[i] = np.nan
                     self.noiseNCM[i] = np.nan
 
-
-
         else:
             self.log.warning("No valid file, skipping pedestal run")
 
-    def mask_alibava_chips(self, chips_to_keep=(1,2), max_channels = 256):
+    def mask_alibava_chips(self, chips_to_keep=(1, 2), max_channels=256):
         """Defines which chips should be considered"""
         final_channels = np.array([], dtype=np.int)
         for chip in chips_to_keep:
-            start = (chip-1)*128
-            to_keep = np.arange(start, start+128, dtype=np.int)
+            start = (chip - 1) * 128
+            to_keep = np.arange(start, start + 128, dtype=np.int)
             final_channels = np.append(final_channels, to_keep)
 
         # No find the masked channels
         channels = np.arange(max_channels, dtype=np.int)
         return final_channels, np.setdiff1d(channels, final_channels)
-
 
     def detect_noisy_strips(self, Noise, Noise_cut):
         """Detect noisy strips (Noise > self.median_noise + Noise_cut) and
@@ -123,23 +136,27 @@ class NoiseAnalysis:
         :param pedestal: the pedestal
         :param tot_noise: bool if you want the tot_noise
         :return:
-        
+
         Written by Dominic Bloech
         """
         # Calculate the common mode noise for every channel
         # Get the signal from event and subtract pedestal
         cm = np.subtract(events, pedestal, dtype=np.float32)
         # Calculate the common mode
-        keep = np.array(list(range(0,len(events))))
-        for i in range(3): # Go over 3 times to get even the less dominant outliers
+        keep = np.array(list(range(0, len(events))))
+        for i in range(3):  # Go over 3 times to get even the less dominant outliers
             CMsig = np.std(cm[keep], axis=1)
             # Now calculate the mean from the cm to get the actual common mode noise
             CMnoise = np.mean(cm[keep], axis=1)
             # Find common mode which is lower/higher than 1 sigma
-            keep = np.where(np.logical_and(CMnoise<(CMnoise+2.5*CMsig), CMnoise>(CMnoise-2.5*CMsig)))[0]
+            keep = np.where(
+                np.logical_and(
+                    CMnoise < (CMnoise + 2.5 * CMsig), CMnoise > (CMnoise - 2.5 * CMsig)
+                )
+            )[0]
         # Calculate the noise of channels
         # Subtract the common mode noise --> Signal[arraylike] - pedestal[arraylike] - Common mode
-        score = np.subtract(cm[keep], CMnoise[keep][:,None], dtype=np.float32)
+        score = np.subtract(cm[keep], CMnoise[keep][:, None], dtype=np.float32)
         # This is a trick with the dimensions of ndarrays, score = shape[ (x,y) - x,1 ]
         # is possible otherwise a loop is the only way
         noise = np.std(score, axis=0)
